6806edcad6d40907c3a51b39356b4986eb52e642
bgsana <bgs.ana08@gmail.com>
2025-10-11 18:10:26 -0300

    1º Commit - Realizado SRP

---
+++ SOLID/SRP/Solução/SCompra.cs
@@ -0,0 +1,26 @@
+namespace SOLID.SRP.Solução;
+
+internal class SCompra
+{
+    public void RealizarCompra()
+    {
+        //Usuário realiza a compra
+    }
+}
+
+internal class SPagamento
+{
+    public void EfetuarPagamento()
+    {
+        //Usuário efetua pagamento
+    }
+}
+
+internal class SConfirmacao
+{
+    public void EnviarConfirmacao()
+    {
+        //O sistema envia uma comprovação de pagamento ao usuário
+    }
+}
+
---
+++ SOLID/SRP/Violação/VCompra.cs
@@ -0,0 +1,17 @@
+namespace SOLID.SRP.Violação;
+
+internal class VCompra
+{
+    public void RealizarCompra()
+    {
+        //usuário realiza a compra
+    }
+    public void EfetuarPagamento()
+    {
+        //usuário efetua pagamento
+    }
+    public void EnviarConfirmação()
+    {
+        //sistema envia email de confirmação
+    }
+}
